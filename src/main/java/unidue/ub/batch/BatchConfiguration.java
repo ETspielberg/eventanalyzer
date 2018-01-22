afa3120b65799647f826af8e7c8561880b5a09f7
Eike Spielberg <Eike.Spielberg@uni-due.de>
2018-01-22 08:03:48 +0100

    restricting SUSHI query to one month fopr update

--- src/main/java/unidue/ub/batch/BatchConfiguration.java
+++ src/main/java/unidue/ub/batch/BatchConfiguration.java
@@ -2,16 +2,18 @@ package unidue.ub.batch;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class BatchConfiguration {
 
     @Bean
     public TaskExecutor taskExecutor() {
-        SimpleAsyncTaskExecutor executor = new SimpleAsyncTaskExecutor();
-        executor.setConcurrencyLimit(50);
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
         return executor;
     }
 }
