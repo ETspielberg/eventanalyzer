272898c5cc0441cceeea39fd9f029772d6f8e80f
Eike Spielberg <Eike.Spielberg@uni-due.de>
2018-01-24 07:06:59 +0100

    manual release of http connections

--- src/main/java/unidue/ub/batch/DataWriter.java
+++ src/main/java/unidue/ub/batch/DataWriter.java
@@ -2,8 +2,6 @@ package unidue.ub.batch;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpConnectionManager;
-import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.RequestEntity;
 import org.apache.commons.httpclient.methods.StringRequestEntity;
@@ -20,11 +18,10 @@ public class DataWriter implements ItemWriter {
 
     @Override
     public void write(List list) throws Exception {
-        HttpConnectionManager httpConnectionManager = new MultiThreadedHttpConnectionManager();
         int succesfullPosts = 0;
         for (Object object : list) {
             String json = mapper.writeValueAsString(object);
-            HttpClient client = new HttpClient(httpConnectionManager);
+            HttpClient client = new HttpClient();
             String objectType = object.getClass().getSimpleName();
             PostMethod post = new PostMethod("http://localhost:8082/api/data/" + objectType.toLowerCase());
             RequestEntity entity = new StringRequestEntity(json, "application/json", null);
@@ -33,6 +30,7 @@ public class DataWriter implements ItemWriter {
             if (status == 201)
                 succesfullPosts++;
             //else log.info("posted " + objectType + " to api/data/" + objectType.toLowerCase() + " with return status " + status);
+            post.releaseConnection();
         }
         log.info("successfully posted " + succesfullPosts + " of " + list.size() + " counter data.");
     }
